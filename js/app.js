69edbb29be8cb6c0b9fae4cd8ea9956b3b8bca58
digaodev <rodrigo.rb.dev@gmail.com>
2018-01-09 13:44:46 -0200

    Feature: adjust html markup to be added via js. Add functions to shuffle the cards and insert them  into the deck. Add function to show /hide the selected card.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,27 @@
 /*
  * Create a list that holds all of your cards
  */
+const cards = [
+  'fa-diamond',
+  'fa-diamond',
+  'fa-paper-plane-o',
+  'fa-paper-plane-o',
+  'fa-anchor',
+  'fa-anchor',
+  'fa-bolt',
+  'fa-bolt',
+  'fa-cube',
+  'fa-cube',
+  'fa-leaf',
+  'fa-leaf',
+  'fa-bicycle',
+  'fa-bicycle',
+  'fa-bomb',
+  'fa-bomb'
+];
 
+let cardsOpened = [];
+let cardsMatched = [];
 
 /*
  * Display the cards on the page
@@ -9,22 +29,40 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+shuffle(cards);
+
+const cardsHTML = cards
+  .map((card, index) => {
+    return `
+        <li class="card"
+          data-card-type="${card}"
+          id="${index}">
+                <i class="fa ${card}"></i>
+        </li>
+    `;
+  })
+  .join('');
+
+const deck = document.querySelector('.deck');
+
+deck.innerHTML = cardsHTML;
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
-
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -36,3 +74,48 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+deck.addEventListener('click', respondToTheClick);
+
+function respondToTheClick(evt) {
+  const selectedCard = evt.target;
+
+  if (evt.target !== this) {
+    toggleShowCard(evt.target);
+    toggleOpenCard(evt.target);
+
+    if (!isCardOpened(selectedCard)) {
+      addToCardsOpened(evt.target);
+    } else {
+      removeFromCardsOpened(evt.target);
+    }
+  }
+
+  console.log(cardsOpened);
+}
+
+function isCardOpened(card) {
+  if (cardsOpened.length > 0) {
+    return !!cardsOpened.find(selectedCard => card.id === selectedCard.id);
+  }
+}
+
+function addToCardsOpened(card) {
+  if (card.id) {
+    cardsOpened = [
+      ...cardsOpened,
+      { id: card.id, card: card.dataset.cardType }
+    ];
+  }
+}
+
+function removeFromCardsOpened(card) {
+  cardsOpened = cardsOpened.filter(filteredCard => card.id !== filteredCard.id);
+}
+
+function toggleShowCard(elem) {
+  elem.classList.toggle('show');
+}
+
+function toggleOpenCard(elem) {
+  elem.classList.toggle('open');
+}
